a5c790e30662e795554856d05e1df6182a11cadc
ac-menon <Achuth.Menon@ambulance.qld.gov.au>
2025-11-07 15:29:55 +1000

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,23 +1,11 @@
-# Compute Instances
-data "template_file" "user_data" {
-  template = file("web-script.sh")
-}
-
 resource "oci_core_instance" "web-01" {
   availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1]["name"]
   compartment_id      = var.compartment_ocid
   display_name        = "Web-Server-01"
   shape               = var.instance_shape
-  shape_config {
-    memory_in_gbs = var.instance_flex_shape_memory
-    ocpus         = var.instance_flex_shape_ocpus
-  }
+  subnet_id           = oci_core_subnet.subnet.id
+  fault_domain = "FAULT-DOMAIN-1"
 
-
-  create_vnic_details {
-    subnet_id    = oci_core_subnet.subnet.id
-    display_name = "web-server-01"
-  }
   source_details {
     source_type             = "image"
     source_id               = lookup(data.oci_core_images.compute_images.images[0], "id")
@@ -36,15 +24,9 @@ resource "oci_core_instance" "web-02" {
   compartment_id      = var.compartment_ocid
   display_name        = "Web-Server-02"
   shape               = var.instance_shape
-  shape_config {
-    memory_in_gbs = var.instance_flex_shape_memory
-    ocpus         = var.instance_flex_shape_ocpus
-  }
+  subnet_id           = oci_core_subnet.subnet.id
+  fault_domain = "FAULT-DOMAIN-2"
 
-  create_vnic_details {
-    subnet_id    = oci_core_subnet.subnet.id
-    display_name = "web-server-02"
-  }
   source_details {
     source_type             = "image"
     source_id               = lookup(data.oci_core_images.compute_images.images[0], "id")
