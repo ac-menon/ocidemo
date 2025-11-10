450b3072001ef0497be39aacdccb1c20bbe9b5e6
ac-menon <Achuth.Menon@ambulance.qld.gov.au>
2025-11-11 09:57:06 +1000

    Create web-script01.sh

---
+++ web-script01.sh
@@ -0,0 +1,10 @@
+sudo yum install httpd -y
+sudo setsebool -P httpd_can_network_connect 1
+sudo setsebool -P httpd_unified 1
+sudo apachectl start
+sudo systemctl enable httpd
+sudo apachectl configtest
+sudo firewall-offline-cmd --add-port=80/tcp
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --reload
+sudo bash -c 'echo This is compute test page from 01 >> /var/www/html/index.html'
